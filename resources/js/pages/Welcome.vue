5bc2d4f869146537f8052b7a73c5233b4fb49ac5
miusarname2 <oscarmauricio631@gmail.com>
2025-10-04 15:49:58 -0500

    feat: :sparkles: Se cambio

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -139,7 +139,7 @@ import { Head, Link } from '@inertiajs/vue3';
                                 :href="login()"
                                 class="inline-block rounded-sm border border-black bg-[#1b1b18] px-5 py-1.5 text-sm leading-normal text-white hover:border-black hover:bg-black dark:border-[#eeeeec] dark:bg-[#eeeeec] dark:text-[#1C1C1A] dark:hover:border-white dark:hover:bg-white"
                             >
-                                Iniciar Sesión
+                                Log In
                             </Link>
                         </li>
                     </ul>
